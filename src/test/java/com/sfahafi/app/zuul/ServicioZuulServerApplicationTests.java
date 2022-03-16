640d9cdf382b8c4e76c73fa0f50600746410c716
ubuntu <ubuntu@ubuntu-Lenovo-B40-70>
2022-03-16 17:28:37 -0300

    Primer commit

---
+++ src/test/java/com/sfahafi/app/zuul/ServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfahafi.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
